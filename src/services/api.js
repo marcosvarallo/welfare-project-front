3bb67fc781419419ec8dbadcf54ed1694a45687f
Marcos Varallo <158823595+marcosvarallo@users.noreply.github.com>
2025-01-13 08:51:31 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://welfare-backend.herokuapp.com",
+  baseURL: "https://welfare-46ef9eb7f9c2.herokuapp.com",
 });
 
 export default api;
