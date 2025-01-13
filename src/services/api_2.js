96bf162ffd24173156b08c8dd15ea3e0f7437e03
Marcos Varallo <158823595+marcosvarallo@users.noreply.github.com>
2025-01-13 08:51:52 -0300

    Update api_2.js

--- src/services/api_2.js
+++ src/services/api_2.js
@@ -1,5 +1,5 @@
 const api = (route, data) =>
-  fetch(`https://welfare-backend.herokuapp.com/api/v1${route}`, {
+  fetch(`https://welfare-46ef9eb7f9c2.herokuapp.com/api/v1${route}`, {
     method: data?.method || "GET",
     headers: { "Content-Type": "application/json" },
     ...data,
